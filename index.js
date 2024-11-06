62469237258f4ff457e6f486e6b50319416331df
Alicia Munhoz <aliciamfc96@gmail.com>
2024-11-06 19:39:24 -0300

    Adicionar rotas para servir arquivos HTML estáticos

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require('express')
 const app = express()
 
+app.use(express.static('public'))
+
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.sendFile(__dirname + '/public/index.html')
+})
+
+app.get('/note/:id', (req, res) => {
+    res.sendFile(__dirname + '/public/note.html')
 })
 
 const port = 3000
